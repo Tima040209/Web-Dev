39fe535ecfc011f722e070717cf9e9bf8d6c71a1
Tima040209 <98690375+Tima040209@users.noreply.github.com>
2023-02-15 15:50:22 +0600

    Add files via upload

---
+++ Lab3/Javascript File/dateAndTime.js
@@ -0,0 +1,83 @@
+//1-task
+let d1 = new Date(2012, 1, 20, 3, 12);
+alert( d1 );
+//2-task
+function getWeekDay(date) {
+  let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+
+  return days[date.getDay()];
+}
+
+let date = new Date(2014, 0, 3); 
+alert( getWeekDay(date) );
+//3-task
+function getLocalDay(date) {
+
+    let day = date.getDay();
+  
+    if (day == 0) { 
+      day = 7;
+    }
+  
+    return day;
+  }
+//4-tsak
+function getDateAgo(date, days) {
+    let dateCopy = new Date(date);
+  
+    dateCopy.setDate(date.getDate() - days);
+    return dateCopy.getDate();
+  }
+  
+  let date = new Date(2015, 0, 2);
+  
+  alert( getDateAgo(date, 1) ); 
+  alert( getDateAgo(date, 2) ); 
+  alert( getDateAgo(date, 365) );
+  //5-task
+  function getLastDayOfMonth(year, month) {
+    let date = new Date(year, month + 1, 0);
+    return date.getDate();
+  }
+  
+  alert( getLastDayOfMonth(2012, 0) ); 
+  alert( getLastDayOfMonth(2012, 1) ); 
+  alert( getLastDayOfMonth(2013, 1) ); 
+  //6-task
+  function getSecondsToday() {
+    let now = new Date();
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  
+    let diff = now - today; 
+    return Math.round(diff / 1000); 
+  }
+  
+  alert( getSecondsToday() 
+  //7-task
+  function formatDate(date) {
+    let dayOfMonth = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let diffMs = new Date() - date;
+    let diffSec = Math.round(diffMs / 1000);
+    let diffMin = diffSec / 60;
+    let diffHour = diffMin / 60;
+  
+    year = year.toString().slice(-2);
+    month = month < 10 ? '0' + month : month;
+    dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+    hour = hour < 10 ? '0' + hour : hour;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+  
+    if (diffSec < 1) {
+      return 'right now';
+    } else if (diffMin < 1) {
+      return `${diffSec} sec. ago`
+    } else if (diffHour < 1) {
+      return `${diffMin} min. ago`
+    } else {
+      return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
+    }
+  }
---
+++ Lab3/Javascript File/json.js
@@ -0,0 +1,24 @@
+//1-trask
+let user = {
+    name: "John Smith",
+    age: 35
+  };
+  
+  let user2 = JSON.parse(JSON.stringify(user));
+//2-task
+let room = {
+    number: 23
+  };
+  
+  let meetup = {
+    title: "Conference",
+    occupiedBy: [{name: "John"}, {name: "Alice"}],
+    place: room
+  };
+  
+  room.occupiedBy = meetup;
+  meetup.self = meetup;
+  
+  alert( JSON.stringify(meetup, function replacer(key, value) {
+    return (key != "" && value == meetup) ? undefined : value;
+  }));
