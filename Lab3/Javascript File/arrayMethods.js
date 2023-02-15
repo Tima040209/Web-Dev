575d880b5f7e7ba31252cb577f350dc45a88e505
Tima040209 <98690375+Tima040209@users.noreply.github.com>
2023-02-15 15:40:31 +0600

    Add files via upload

---
+++ Lab3/Javascript File/arrayMethods.js
@@ -0,0 +1,173 @@
+//1-task
+function camelize(str) {
+    return str
+      .split('-')
+      .map(
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+      )
+      .join(''); 
+  }
+//2-task
+function filterRange(arr, a, b) {
+    return arr.filter(item => (a <= item && item <= b));
+  }
+  
+  let arr = [5, 3, 8, 1];
+  
+  let filtered = filterRange(arr, 1, 4);
+  
+  alert( filtered );
+  
+  alert( arr ); 
+  //3-task
+  function filterRangeInPlace(arr, a, b) {
+
+    for (let i = 0; i < arr.length; i++) {
+      let val = arr[i];
+  
+      if (val < a || val > b) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+  
+  }
+  
+  let arr = [5, 3, 8, 1];
+  
+  filterRangeInPlace(arr, 1, 4); 
+  
+  alert( arr );
+//4-task
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+alert( arr );
+//5-task
+function copySorted(arr) {
+    return arr.slice().sort();
+  }
+  
+  let arr = ["HTML", "JavaScript", "CSS"];
+  
+  let sorted = copySorted(arr);
+  
+  alert( sorted );
+  alert( arr );
+//6-task
+function Calculator() {
+
+    this.methods = {
+      "-": (a, b) => a - b,
+      "+": (a, b) => a + b
+    };
+  
+    this.calculate = function(str) {
+  
+      let split = str.split(' '),
+        a = +split[0],
+        op = split[1],
+        b = +split[2];
+  
+      if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+        return NaN;
+      }
+  
+      return this.methods[op](a, b);
+    };
+  
+    this.addMethod = function(name, func) {
+      this.methods[name] = func;
+    };
+  }
+//7-task
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(item => item.name);
+
+alert( names );
+//8-task
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
+
+
+alert( usersMapped[0].id );
+alert( usersMapped[0].fullName );
+//9-task
+function sortByAge(arr) {
+    arr.sort((a, b) => a.age - b.age);
+  }
+  
+  let john = { name: "John", age: 25 };
+  let pete = { name: "Pete", age: 30 };
+  let mary = { name: "Mary", age: 28 };
+  
+  let arr = [ pete, john, mary ];
+  
+  sortByAge(arr);
+  
+
+  alert(arr[0].name); 
+  alert(arr[1].name); 
+  alert(arr[2].name);
+//10-task
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+  let count = {
+    '123': 0,
+    '132': 0,
+    '213': 0,
+    '231': 0,
+    '321': 0,
+    '312': 0
+  };
+  
+  for (let i = 0; i < 1000000; i++) {
+    let array = [1, 2, 3];
+    shuffle(array);
+    count[array.join('')]++;
+  }
+
+  for (let key in count) {
+    alert(`${key}: ${count[key]}`);
+  }
+//12-task
+function unique(arr) {
+    let result = [];
+  
+    for (let str of arr) {
+      if (!result.includes(str)) {
+        result.push(str);
+      }
+    }
+  
+    return result;
+  }
+  
+  let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+  
+  alert( unique(strings) );
+  //13-task
+  function groupById(array) {
+    return array.reduce((obj, value) => {
+      obj[value.id] = value;
+      return obj;
+    }, {})
+  }
