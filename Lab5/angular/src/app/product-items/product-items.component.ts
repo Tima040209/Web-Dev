482062c3c70edd004679c9c30ca24181a4762e22
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-03-01 15:54:06 +0600

    did Input and Output functions

--- Lab5/angular/src/app/product-items/product-items.component.ts
+++ Lab5/angular/src/app/product-items/product-items.component.ts
@@ -1,15 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { products } from '../products';
+import { Product } from '../products';
 @Component({
   selector: 'app-product-items',
   templateUrl: './product-items.component.html',
   styleUrls: ['./product-items.component.css']
 })
 export class ProductItemsComponent {
-@Input() product: products | undefined;
+@Input() product: Product | undefined;
 @Output() shareProduct = new EventEmitter();
 
 share(sh: string){
   this.shareProduct.emit(sh);
+}
+onNotify() {
+  window.alert('You will be notified when the product goes on sale');
 }
 }
--- Lab5/angular/src/app/product-list/product-list.component.ts
+++ Lab5/angular/src/app/product-list/product-list.component.ts
@@ -25,9 +25,6 @@ export class ProductListComponent {
     window.open(`https://t.me/share/url?url=${sh}&text=xssxcfscxscsc`);
   }
 
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale');
-  }
   
 }
 
