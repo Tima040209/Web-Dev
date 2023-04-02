a487654a60c2bee07e56b57c01977e4668bddae2
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-02 18:01:36 +0600

    add product and category

--- lab8/api/urls.py
+++ lab8/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from api import views
 urlpatterns = [
-    path('home/',views.home)
+    path('products/',views.product_list),
+    path('products/<int:id>/',views.product_detail)
 ]
--- lab8/api/views.py
+++ lab8/api/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse,JsonResponse
+from api.models import Product
 
 # Create your views here.
-def home(request):
-    return HttpResponse('home page')
+products= Product.objects.all()
+def product_list(request):
+    products_json = [p.to_json() for p in products]
+    return JsonResponse(products_json,safe=False)
+def product_detail(request,id):
+
+    for product in products:
+        if product[id]==id:
+            return JsonResponse(product.to_json())
+    return JsonResponse({'error': 'Product not found'})
