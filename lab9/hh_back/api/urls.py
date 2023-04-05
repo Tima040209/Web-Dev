d4de883c1a1339f89e894c31da0189d82d6fb9bc
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-05 17:49:21 +0600

    made models

--- lab9/hh_back/api/urls.py
+++ lab9/hh_back/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from api import views
 urlpatterns = [
-    path('products/',views.product_list),
-    path('products/<int:id>/',views.product_detail),
-    path('category/',views.category_list),
-    path('category/<int:id>/',views.category_detail),
-    path('category/<int:id>/products', views.category_product)
+    path('companies/',views.product_list),
+    path('companies/<int:id>/',views.product_detail),
+    path('companies/int:id>/vacancies/',views.category_list),
+    path('category/vacancies/',views.category_detail),
+    path('category/vacancies/<int:id>/', views.category_product),
+    path('vacancies/<int:id>/'),
+    path('vacancies/top_ten/')
 ]
