00aa50bed663f38ae6be1b972b495d05cddd5eef
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-05 17:35:47 +0600

    did api application

--- lab9/hh_back/api/models.py
+++ lab9/hh_back/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(default='')
+    city = models.CharField(max_length=255)
+    address = models.TextField(default='')
+    def to_json(self):
+        return{
+            'id' : self.id,
+            'name' : self.name,
+            'description' : self.description,
+            'city' : self.city,
+            'address' : self.address
+        }
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(default='')
+    salary = models.FloatField(default=100000)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    def to_json(self):
+        return{
+            'id' : self.id,
+            'name' : self.name,
+            'description' : self.description,
+            'salary' :self.salary,
+        }
