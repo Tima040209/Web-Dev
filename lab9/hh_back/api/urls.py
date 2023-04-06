7d9424d78c043bb31bf2f757c6e7b4a82d194de4
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-06 13:12:27 +0600

    made urls

--- lab9/hh_back/api/urls.py
+++ lab9/hh_back/api/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('companies/',views.company_list),
-    # path('companies/<int:id>/',views.product_detail),
-    # path('companies/int:id>/vacancies/',views.category_list),
-    # path('category/vacancies/',views.category_detail),
-    # path('category/vacancies/<int:id>/', views.category_product),
-    # path('vacancies/<int:id>/'),
-    # path('vacancies/top_ten/')
+    path('companies/<int:id>/',views.company_detail),
+    path('companies/int:id>/vacancies/',views.company_vacancies),
+    path('vacancies/',views.vacancy_list),
+    path('vacancies/<int:id>/',views.vacancy_detail),
+
 ]
--- lab9/hh_back/api/views.py
+++ lab9/hh_back/api/views.py
@@ -6,4 +6,19 @@ def company_list(request):
     companies = Company.objects.all()
     company_json = [c.to_json() for c in companies]
     return JsonResponse(company_json,safe=False)
-
+def vacancy_list(request):
+    vacancies = Vacancy.objects.all()
+    vacancy_json = [v.to_json() for v in vacancies]
+    return JsonResponse(vacancy_json,safe=False)
+def company_detail(request,id):
+        company = Company.objects.get(pk=id)
+        return JsonResponse(company.to_json())
+def company_vacancies(request,id):
+     vacancies = [v.to_json() for v in Vacancy.objects.filter(company_id = id)]
+     company_vacancies = {
+          'vacancies' : vacancies
+     }
+     return JsonResponse(company_vacancies)
+def vacancy_detail(request,id) :
+     vacancy  = Vacancy.objects.get(pk = id)
+     return JsonResponse(vacancy.to_json())
