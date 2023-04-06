a5ff5709dbbd40c7d2d310b2dc9cf11edd8551d5
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-06 10:05:54 +0600

    made views

--- lab9/hh_back/api/urls.py
+++ lab9/hh_back/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from api import views
 urlpatterns = [
-    path('companies/',views.product_list),
-    path('companies/<int:id>/',views.product_detail),
-    path('companies/int:id>/vacancies/',views.category_list),
-    path('category/vacancies/',views.category_detail),
-    path('category/vacancies/<int:id>/', views.category_product),
-    path('vacancies/<int:id>/'),
-    path('vacancies/top_ten/')
+    path('companies/',views.company_list),
+    # path('companies/<int:id>/',views.product_detail),
+    # path('companies/int:id>/vacancies/',views.category_list),
+    # path('category/vacancies/',views.category_detail),
+    # path('category/vacancies/<int:id>/', views.category_product),
+    # path('vacancies/<int:id>/'),
+    # path('vacancies/top_ten/')
 ]
--- lab9/hh_back/api/views.py
+++ lab9/hh_back/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http.response import JsonResponse
+from api.models import Company,Vacancy
 # Create your views here.
+def company_list(request):
+    companies = Company.objects.all()
+    company_json = [c.to_json() for c in companies]
+    return JsonResponse(company_json,safe=False)
+
