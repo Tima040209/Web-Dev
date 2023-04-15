0b083e47debef5b270ad7252e1532de1ce8b1d6d
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-15 16:14:10 +0600

    made interfaces

--- lab10/hh_front/src/app/app.component.ts
+++ lab10/hh_front/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Company, Vacancy } from './models';
+import { CompanyService } from './company.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hh_front';
+
+  companies : Company[] = [];
+  vacancies : Vacancy[]= [];
+  
+  constructor(private companyService: CompanyService){
+
+  }
+  ngOnInit(){
+    this.companyService.getCompanies().subscribe((data:Company[])=>{
+      console.log(data);
+    })
+
+  }
 }
